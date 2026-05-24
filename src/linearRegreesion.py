10b89f297fb78a4897f245dbdc832a65fbbc0b17
Ivan <evansbegue@gmail.com>
2026-05-24 20:31:37 +0400

    update Linear Regression

--- src/linearRegreesion.py
+++ src/linearRegreesion.py
@@ -10,19 +10,15 @@ df = pd.read_csv(
     r"C:\Users\ivans\Desktop\All\MSC\Supervised_Machine_learning\Assignment\AIML_assingment_1\study-hours-score-predictor\data\marks_v2.csv"
 )
 
-<<<<<<< HEAD
 # Split the dataset into:
 # - Features (hours studied)
 # - Target variable (marks obtained)
 x_train, x_test, y_train, y_test = train_test_split(
     df['hours'],
     df['marks'],
-    test_size=0.2, # test_size=0.2  80% data for training and 20% for testing
+    test_size=0.1, # test_size=0.10  90% data for training and 10% for testing
     random_state=42 # random_state=42 ensures reproducible results
 )
-=======
-df=pd.read_csv(r"..\study-hours-score-predictor\data\marks_v2.csv")
->>>>>>> 420395c9bcdf53f98f99531a21fd257ff3a70f67
 
 # Convert pandas Series into NumPy arrays
 # reshape((-1,1)) converts the data into a 2D array
@@ -40,6 +36,16 @@ lrm.fit(x_train, y_train)
 
 # Use the trained model to predict marks for test data
 predictions = lrm.predict(x_test)
+
+
+# MODEL EVALUATION
+
+print("MAE:", metrics.mean_absolute_error(y_test, predictions))
+print("MSE:", metrics.mean_squared_error(y_test, predictions))
+print("RMSE:", np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+print("R2 Score:", metrics.r2_score(y_test, predictions))
+
+
 
 # Create a figure for visualization (width of figure = 8 && height of the figure= 5)
 plt.figure(figsize=(8, 5)) 
